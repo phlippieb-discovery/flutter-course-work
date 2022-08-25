81e25e57ba6c0a995d1cc07a315cd47bc26a5d14
Phlippie <pbosman@retrorabbit.co.za>
2022-08-25 19:22:21 +0200

    Complete contact card app

--- section6/mi_card_flutter/lib/main.dart
+++ section6/mi_card_flutter/lib/main.dart
@@ -13,29 +13,100 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child:
-              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-            Container(
-              width: 100.0,
-              color: Colors.red,
-            ),
-            Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.yellow,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Image
+              CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 80,
+                backgroundImage: NetworkImage(
+                    'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixid=MnwzNTg2Mzd8MHwxfHNlYXJjaHw0fHxwcm9maWxlJTIwcGljdHVyZXxlbnwwfHx8fDE2NjE0NDMwODI&ixlib=rb-1.2.1'),
               ),
-              Container(
-                width: 100.0,
-                height: 100.0,
-                color: Colors.green,
+
+              // Heading
+              Text(
+                "Some Guy idk",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ]),
-            Container(
-              width: 100,
-              color: Colors.blue,
-            ),
-          ]),
+
+              // Subheading
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('HE DO BE CODIN THO',
+                    style: TextStyle(
+                        color: Colors.teal.shade100,
+                        fontFamily: 'Source Sans',
+                        fontSize: 14,
+                        letterSpacing: 2,
+                        fontWeight: FontWeight.bold)),
+              ),
+
+              // Divider
+              SizedBox(
+                width: 80,
+                height: 50,
+                child: Divider(
+                  color: Colors.white,
+                  thickness: 3,
+                ),
+              ),
+
+              // Phone card
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 24,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade500,
+                    size: 32,
+                  ),
+                  title: Text(
+                    "508-455-9667",
+                    style: TextStyle(
+                      fontFamily: 'Source Sans',
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+
+              // Email card
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 24,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade500,
+                    size: 32,
+                  ),
+                  title: Text(
+                    "Dariana_Stroman18@gmail.com",
+                    style: TextStyle(
+                      fontFamily: 'Source Sans',
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

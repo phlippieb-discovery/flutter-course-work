0ec8d7b147b6e539125b3d6cba070b6c5c71c3ac
Phlippie <pbosman@retrorabbit.co.za>
2022-08-26 16:14:17 +0200

    Section 8 - 8 ball app

--- section8/magic-8-ball-flutter/lib/main.dart
+++ section8/magic-8-ball-flutter/lib/main.dart
@@ -1,7 +1,45 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Ask me something"),
+        ),
+        backgroundColor: Colors.blue,
+        body: MainApp(),
+      ),
+    ),
+  );
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({Key key}) : super(key: key);
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int n = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        child: Image.asset('images/ball$n.png'),
+        onPressed: () {
+          setState(() {
+            updateBall();
+          });
+        },
       ),
     );
+  }
+
+  void updateBall() {
+    n = Random().nextInt(5) + 1;
+  }
+}

18a8983c50e669b52634a5a3f967f2bf475d8a33
Phlippie <pbosman@retrorabbit.co.za>
2022-08-29 12:43:35 +0200

    Section 9 - xylophone app

--- section9/xylophone-flutter/lib/main.dart
+++ section9/xylophone-flutter/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -7,10 +8,46 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              makeButton(1),
+              makeButton(2),
+              makeButton(3),
+              makeButton(4),
+              makeButton(5),
+              makeButton(6),
+              makeButton(7),
+            ],
+          ),
         ),
       ),
     );
   }
+
+  Widget makeButton(int number) => Expanded(
+        child: Container(
+          color: getColor(number: number),
+          child: TextButton(
+            onPressed: () {
+              final player = AudioCache();
+              player.play(getNoteName(number: number));
+            },
+          ),
+        ),
+      );
+
+  String getNoteName({int number}) => 'note$number.wav';
+
+  Color getColor({int number}) => [
+        Colors.red,
+        Colors.orange,
+        Colors.yellow,
+        Colors.green,
+        Colors.blue,
+        Colors.indigo,
+        Colors.purple
+      ][number - 1];
 }

163144b414042bc115cfaf48bf1e2ab7838dd2d8
Phlippie <pbosman@retrorabbit.co.za>
2022-08-29 17:12:16 +0200

    Section 10 - own implementation

--- section10/quizzler-flutter/lib/main.dart
+++ section10/quizzler-flutter/lib/main.dart
@@ -28,66 +28,107 @@ class _QuizPageState extends State<QuizPage> {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Expanded(
-          flex: 5,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                'This is where the question text will go.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: currentIndex < questions.length
+            ? <Widget>[
+                Expanded(
+                  flex: 5,
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Center(
+                      child: Text(
+                        questions[currentIndex],
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: FlatButton(
+                      textColor: Colors.white,
+                      color: Colors.green,
+                      child: Text(
+                        'True',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          scores.add(answers[currentIndex] == true);
+                          currentIndex++;
+                        });
+                      },
+                    ),
+                  ),
                 ),
-              ),
-              onPressed: () {
-                //The user picked true.
-              },
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: FlatButton(
+                      color: Colors.red,
+                      child: Text(
+                        'False',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          scores.add(answers[currentIndex] == false);
+                          currentIndex++;
+                        });
+                      },
+                    ),
+                  ),
                 ),
-              ),
-              onPressed: () {
-                //The user picked false.
-              },
-            ),
-          ),
-        ),
-        //TODO: Add a Row here as your score keeper
-      ],
-    );
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: getScoreIcons(),
+                  ),
+                )
+              ]
+            : [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: getScoreIcons(),
+                ),
+              ]);
   }
+
+  int currentIndex = 0;
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+
+  List<bool> answers = [
+    false,
+    true,
+    true,
+  ];
+
+  List<bool> scores = [];
+
+  List<Widget> getScoreIcons() => scores
+      .map((score) => Icon(
+            score == true ? Icons.check : Icons.close,
+            color: score == true ? Colors.green : Colors.red,
+          ))
+      .toList();
 }
 
 /*

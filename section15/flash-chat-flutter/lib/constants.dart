a666a4b25950429005008785acffa85dc4a1bf3f
Phlippie <pbosman@retrorabbit.co.za>
2022-09-13 11:38:13 +0200

    Section 15 progress

--- section15/flash-chat-flutter/lib/constants.dart
+++ section15/flash-chat-flutter/lib/constants.dart
@@ -19,3 +19,32 @@ const kMessageContainerDecoration = BoxDecoration(
 );
 
 const String kZapLogoHeroTag = 'zapLogoHeroTag';
+
+const InputDecoration kInputDecoration = InputDecoration(
+  hintText: 'Enter your email',
+  hintStyle: kInputHintTextStyle,
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueAccent, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Colors.blueAccent, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+);
+
+const kInputHintTextStyle = TextStyle(
+  color: Colors.grey,
+  fontWeight: FontWeight.w300,
+  fontSize: 18.0,
+);
+
+const kInputTextStyle = TextStyle(
+  color: Colors.black,
+  fontWeight: FontWeight.w500,
+  fontSize: 18.0,
+);
--- section15/flash-chat-flutter/lib/screens/welcome_screen.dart
+++ section15/flash-chat-flutter/lib/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/my_button.dart';
 import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
@@ -42,42 +43,20 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
             SizedBox(
               height: 48.0,
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                elevation: 5.0,
-                color: Colors.lightBlueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                child: MaterialButton(
-                  onPressed: () {
-                    //Go to login screen.
-                    Navigator.pushNamed(context, LoginScreen.route);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Log In',
-                  ),
-                ),
-              ),
+            MyButton(
+              text: 'Log In',
+              color: Colors.lightBlue,
+              onPressed: () {
+                //Go to login screen.
+                Navigator.pushNamed(context, LoginScreen.route);
+              },
             ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 16.0),
-              child: Material(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(30.0),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    Navigator.pushNamed(context, RegistrationScreen.route);
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: Text(
-                    'Register',
-                  ),
-                ),
-              ),
+            MyButton(
+              text: 'Register',
+              color: Colors.blueAccent,
+              onPressed: () {
+                Navigator.pushNamed(context, RegistrationScreen.route);
+              },
             ),
           ],
         ),
